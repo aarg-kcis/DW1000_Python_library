3ab907042341c93a6dda79084bfb9f98505ad459
Abhay <abhayrawat2695@gmail.com>
2018-05-18 01:15:42 +0530

    time read during interrupt call

--- DW1000RangingAnchor.py
+++ DW1000RangingAnchor.py
@@ -49,8 +49,14 @@ def handleReceived():
     This is a callback called from the module's interrupt handler when a reception was successful.
     It sets the received receivedAck as True so the loop can continue.
     """
-    global receivedAck
+    global receivedAck, data, timeRangeReceivedTS, timePollReceivedTS
     receivedAck = True
+    data = DW1000.getData(LEN_DATA)
+    if data[0] == C.POLL:
+        timePollReceivedTS = DW1000.getReceiveTimestamp()
+    if data[0] == C.RANGE:
+        timeRangeReceivedTS = DW1000.getReceiveTimestamp()
+
 
 
 def noteActivity():
@@ -140,8 +146,6 @@ def computeRangeAsymmetric():
     print "ROUND 1: ", round1,reply1
     print "ROUND 2: ", round2,reply2
     timeComputedRangeTS = (round1 * round2 - reply1 * reply2) / (round1 + round2 + reply1 + reply2)
-    # timeComputedRangeTS = (round2 + round1 - reply1 - reply2)/2
-
 
 def loop():
     global sentAck, receivedAck, timePollAckSentTS, timePollReceivedTS, timePollSentTS, timePollAckReceivedTS, timeRangeReceivedTS, protocolFailed, data, expectedMsgId, timeRangeSentTS, SEQ_NO
@@ -162,7 +166,6 @@ def loop():
 
     if receivedAck:
         receivedAck = False
-        data = DW1000.getData(LEN_DATA)
         print "getting data ", data
         msgId = data[0]
         if msgId != expectedMsgId:
@@ -170,14 +173,13 @@ def loop():
             protocolFailed = True
         if msgId == C.POLL:
             protocolFailed = False
-            timePollReceivedTS = DW1000.getReceiveTimestamp()
             print "timePollReceivedTS for SEQ_NO {} : {}".format(SEQ_NO, timePollReceivedTS)
             expectedMsgId = C.RANGE
             transmitPollAck()
             noteActivity()
         elif msgId == C.RANGE:
-            timeRangeReceivedTS = DW1000.getReceiveTimestamp()
             expectedMsgId = C.POLL
+            print "timeRangeReceivedTS for SEQ_NO {} : {}".format(SEQ_NO, timeRangeReceivedTS)
             if protocolFailed == False:
                 timePollSentTS = DW1000.getTimeStamp(data, 1)
                 timePollAckReceivedTS = DW1000.getTimeStamp(data, 6)
